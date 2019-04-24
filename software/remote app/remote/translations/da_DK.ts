1672f0885027244a15a5dcbdd9dc0184bd49f862
Marton <hello@martonborzak.com>
2019-04-25 00:15:36 +0200

    New translations en_US.ts (Danish)

--- software/remote app/remote/translations/da_DK.ts
+++ software/remote app/remote/translations/da_DK.ts
@@ -28,13 +28,13 @@
     <name>Button</name>
     <message>
       <location filename="../components/light/Button.qml" line="405"/>
-      <source>Remove from favorits</source>
-      <translation type="unfinished">Remove from favorits</translation>
+      <source>Remove from favorites</source>
+      <translation type="unfinished">Remove from favorites</translation>
     </message>
     <message>
       <location filename="../components/light/Button.qml" line="405"/>
-      <source>Add to favorits</source>
-      <translation type="unfinished">Add to favorits</translation>
+      <source>Add to favorites</source>
+      <translation type="unfinished">Add to favorites</translation>
     </message>
   </context>
   <context>
