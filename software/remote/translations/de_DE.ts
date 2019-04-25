fdb826348a77b9f6da3f1f7ed8b0e2a234dc4d39
Marton <hello@martonborzak.com>
2019-04-25 23:20:18 +0200

    New translations en_US.ts (German)

---
+++ software/remote/translations/de_DE.ts
@@ -0,0 +1,296 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+  <context>
+    <name>Battery</name>
+    <message>
+      <location filename="../basic_ui/settings/Battery.qml" line="17"/>
+      <source>Battery</source>
+      <translation>Batterie</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Battery.qml" line="38"/>
+      <source>Battery charge</source>
+      <translation>Batterieladung</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Battery.qml" line="74"/>
+      <source>Battery health</source>
+      <translation>Batteriezustand</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Battery.qml" line="110"/>
+      <source>Battery voltage</source>
+      <translation>Batteriespannung</translation>
+    </message>
+  </context>
+  <context>
+    <name>Button</name>
+    <message>
+      <location filename="../components/light/Button.qml" line="405"/>
+      <source>Remove from favorites</source>
+      <translation>Aus Favoriten entfernen</translation>
+    </message>
+    <message>
+      <location filename="../components/light/Button.qml" line="405"/>
+      <source>Add to favorites</source>
+      <translation>Zu Favoriten hinzufügen</translation>
+    </message>
+  </context>
+  <context>
+    <name>ChargingScreen</name>
+    <message>
+      <location filename="../basic_ui/ChargingScreen.qml" line="90"/>
+      <source>Charging</source>
+      <translation>aufladen</translation>
+    </message>
+  </context>
+  <context>
+    <name>ConnectionScreen</name>
+    <message>
+      <location filename="../basic_ui/ConnectionScreen.qml" line="21"/>
+      <location filename="../basic_ui/ConnectionScreen.qml" line="154"/>
+      <source>Reconnecting...</source>
+      <translation>Erneut verbinden...</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/ConnectionScreen.qml" line="31"/>
+      <source>Connected</source>
+      <translation>Verbunden</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/ConnectionScreen.qml" line="40"/>
+      <source>Connection failed</source>
+      <translation>Verbindung fehlgeschlagen</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/ConnectionScreen.qml" line="169"/>
+      <source>Try again</source>
+      <translation>Wiederholen</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/ConnectionScreen.qml" line="183"/>
+      <source>Hello</source>
+      <translation>Hallo</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/ConnectionScreen.qml" line="203"/>
+      <source>Wait for it...</source>
+      <translation>Warten sie...</translation>
+    </message>
+  </context>
+  <context>
+    <name>Devices</name>
+    <message>
+      <location filename="../basic_ui/settings/Devices.qml" line="17"/>
+      <source>Devices</source>
+      <translation>Geräte</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Devices.qml" line="38"/>
+      <source>Add new</source>
+      <translation>Neue hinzufügen</translation>
+    </message>
+  </context>
+  <context>
+    <name>Display</name>
+    <message>
+      <location filename="../basic_ui/settings/Display.qml" line="17"/>
+      <source>Display</source>
+      <translation>Bildschirm</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Display.qml" line="38"/>
+      <source>Auto brightness</source>
+      <translation>Auto-Helligkeit</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Display.qml" line="164"/>
+      <source>Dark mode</source>
+      <translation>Dunkler Modus</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Display.qml" line="224"/>
+      <source>Cleaning mode</source>
+      <translation>Reinigungsmodus</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Display.qml" line="239"/>
+      <source>Touch the display for 10 seconds to turn cleaning mode off.</source>
+      <translation>Drücken Sie das Display für 10 Sekunden, um den Reinigungsmodus auszuschalten.</translation>
+    </message>
+  </context>
+  <context>
+    <name>Hubs</name>
+    <message>
+      <location filename="../basic_ui/settings/Hubs.qml" line="17"/>
+      <source>Hubs</source>
+      <translation type="unfinished">Hubs</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Hubs.qml" line="38"/>
+      <source>Add new</source>
+      <translation>Neue hinzufügen</translation>
+    </message>
+  </context>
+  <context>
+    <name>Languages</name>
+    <message>
+      <location filename="../basic_ui/settings/Languages.qml" line="25"/>
+      <source>Languages</source>
+      <translation>Sprachen</translation>
+    </message>
+  </context>
+  <context>
+    <name>MainNavigation</name>
+    <message>
+      <location filename="../basic_ui/main_navigation/MainNavigation.qml" line="65"/>
+      <source>Dashboard</source>
+      <translation type="unfinished">Dashboard</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/main_navigation/MainNavigation.qml" line="74"/>
+      <source>Areas</source>
+      <translation>Bereiche</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/main_navigation/MainNavigation.qml" line="95"/>
+      <source>Settings</source>
+      <translation>Einstellungen</translation>
+    </message>
+  </context>
+  <context>
+    <name>Network</name>
+    <message>
+      <location filename="../basic_ui/settings/Network.qml" line="30"/>
+      <source>Network</source>
+      <translation>Netzwerk</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Network.qml" line="51"/>
+      <source>Wi-Fi signal strength</source>
+      <translation>Wi-Fi Signalstärke</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Network.qml" line="88"/>
+      <source>IP address</source>
+      <translation>IP-Adresse</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/Network.qml" line="124"/>
+      <source>MAC address</source>
+      <translation>MAC-Adresse</translation>
+    </message>
+  </context>
+  <context>
+    <name>PopupLowBattery</name>
+    <message>
+      <location filename="../basic_ui/PopupLowBattery.qml" line="98"/>
+      <source>Low battery</source>
+      <translation>Schwache Batterie</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/PopupLowBattery.qml" line="113"/>
+      <source>Please charge the remote soon</source>
+      <translation>Bitte laden Sie die Fernbedienung bald auf</translation>
+    </message>
+  </context>
+  <context>
+    <name>PowerSaving</name>
+    <message>
+      <location filename="../basic_ui/settings/PowerSaving.qml" line="17"/>
+      <source>Power saving</source>
+      <translation>Energiesparen</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/PowerSaving.qml" line="39"/>
+      <source>Never turn off Wi-Fi</source>
+      <translation>Wi-Fi nie abschalten</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/PowerSaving.qml" line="39"/>
+      <source>Turn off Wi-Fi after %1 minutes of inactivity</source>
+      <translation>Wi-Fi nach %1 Minuten abschalten</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/PowerSaving.qml" line="119"/>
+      <location filename="../basic_ui/settings/PowerSaving.qml" line="234"/>
+      <source>Never</source>
+      <translation>Niemals</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/PowerSaving.qml" line="132"/>
+      <source>60 minutes</source>
+      <translation>60 Minuten</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/PowerSaving.qml" line="154"/>
+      <source>Never turn off the remote</source>
+      <translation>Fernbedienung nie abschalten</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/PowerSaving.qml" line="154"/>
+      <source>Turn off the remote after %1 hours of inactivity</source>
+      <translation>Fernbedienung nach %1 Stunden abschalten</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/PowerSaving.qml" line="247"/>
+      <source>8 hours</source>
+      <translation>8 Stunden</translation>
+    </message>
+  </context>
+  <context>
+    <name>StatusBar</name>
+    <message>
+      <location filename="../basic_ui/StatusBar.qml" line="122"/>
+      <source>New software version is available.</source>
+      <translation>Neue Softwareversion ist verfügbar.</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/StatusBar.qml" line="152"/>
+      <source>Update</source>
+      <translation>aktualisieren</translation>
+    </message>
+  </context>
+  <context>
+    <name>System</name>
+    <message>
+      <location filename="../basic_ui/settings/System.qml" line="33"/>
+      <source>System</source>
+      <translation type="unfinished">System</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/System.qml" line="54"/>
+      <source>Uptime</source>
+      <translation>Betriebszeit</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/System.qml" line="91"/>
+      <source>CPU temperature</source>
+      <translation>CPU-Temperatur</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/System.qml" line="127"/>
+      <source>Reboot</source>
+      <translation>Neu starten</translation>
+    </message>
+    <message>
+      <location filename="../basic_ui/settings/System.qml" line="142"/>
+      <source>Shutdown</source>
+      <translation>Herunterfahren</translation>
+    </message>
+  </context>
+  <context>
+    <name>main</name>
+    <message>
+      <location filename="../main.qml" line="158"/>
+      <source>Lights</source>
+      <translation>Lampen</translation>
+    </message>
+    <message>
+      <location filename="../main.qml" line="158"/>
+      <source>Blinds</source>
+      <translation>Rolläden</translation>
+    </message>
+  </context>
+</TS>
