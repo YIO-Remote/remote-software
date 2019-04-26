f8f9c11e06d7bf19085aa26099a6b28f3bc346ef
Gilles <gilles@webunity.nl>
2019-04-26 13:21:39 +0200

    Removed the need to change config location
    JSON files are copied to output directory and smart detection of config location path.

--- software/remote/sources/main.cpp
+++ software/remote/sources/main.cpp
@@ -2,6 +2,9 @@
 #include <QQmlApplicationEngine>
 #include <QFontDatabase>
 #include <QQmlContext>
+#include <QDebug>
+#include <QFile>
+#include <QFileInfo>
 
 #include "launcher.h"
 #include "jsonfile.h"
@@ -16,6 +19,26 @@ int main(int argc, char *argv[])
     //    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+    // Get the applications dir path and expose it to QML (prevents setting the JSON config variable)
+    QString appPath = app.applicationDirPath();
+    QString macPath = QFileInfo(appPath + "/../../").canonicalPath();
+
+    // In case the translation file does not exist at root level (which is copied via the .pro file)
+    // we are probably running on a mac, which gives a different result for the applicationDirPath
+    // (actually points to '.../remote.app/Contents/MacOS/remote')
+    if ((!QFile::exists(appPath + "/translations.json")) && (QFile::exists(macPath + "/translations.json"))) {
+        appPath = macPath;
+    }
+    engine.rootContext()->setContextProperty("appPath", appPath);
+
+    // Determine configuration path
+    QString configPath = appPath;
+    if (QFile::exists("/mnt/boot/config.json")) {
+        configPath = "/mnt/boot";
+    }
+    engine.rootContext()->setContextProperty("configPath", configPath);
 
     // LOADING FONTS
     QFontDatabase::addApplicationFont(QStringLiteral("/usr/lib/fonts/OpenSans-Regular.ttf"));
@@ -27,7 +50,6 @@ int main(int argc, char *argv[])
     qmlRegisterType<JsonFile>("JsonFile", 1, 0, "JsonFile");
     qmlRegisterType<DisplayControl>("DisplayControl", 1, 0, "DisplayControl");
 
-    QQmlApplicationEngine engine;
 
     // TRANSLATION
     TranslationHandler transHndl(&engine);
@@ -36,7 +58,6 @@ int main(int argc, char *argv[])
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
-
 
     return app.exec();
 }
