5cb0f2885172027cfa7e081c8b518a1b907f85a9
Marton <hello@martonborzak.com>
2019-07-21 19:10:45 +0200

    Update mediaplayer.cpp

--- software/remote/sources/entities/mediaplayer.cpp
+++ software/remote/sources/entities/mediaplayer.cpp
@@ -7,18 +7,7 @@ QString MediaPlayer::Type = "media_player";
 bool MediaPlayer::update(const QVariantMap &attributes)
 {
     bool chg = false;
-//    enum states nst;
-//    if (attributes.value("state").toInt() == 0) {
-//        nst = OFF;
-//    } else if (attributes.value("state").toInt() == 1) {
-//        nst = ON;
-//    } else if (attributes.value("state").toInt() == 2) {
-//        nst = IDLE;
-//    } else if (attributes.value("state").toInt() == 3) {
-//        nst = PLAYING;
-//    }
-
-    if (m_state != static_cast<states>(attributes.value("state").toInt())) { // static_cast<states>(attributes.value("state").toInt())
+    if (m_state != static_cast<states>(attributes.value("state").toInt())) {
         m_state = static_cast<states>(attributes.value("state").toInt());
         if (m_state == PLAYING) {
             emit playing(objectName());
