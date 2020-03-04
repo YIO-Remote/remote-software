f0d107dd8f1048b938d7731faf4be0a32b6ba88a
Markus Zehnder <business@markuszehnder.ch>
2020-03-04 22:18:17 +0100

    Software update enhancements
    - Queueable file downloads
    - Metafile for version information
    - Various fixes

--- sources/filedownload.h
+++ sources/filedownload.h
@@ -1,7 +1,6 @@
 /******************************************************************************
  *
  * Copyright (C) 2020 Markus Zehnder <business@markuszehnder.ch>
- * Copyright (C) 2020 Marton Borzak <hello@martonborzak.com>
  *
  * This file is part of the YIO-Remote software project.
  *
@@ -29,7 +28,14 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QObject>
+#include <QQueue>
 
+/**
+ * @brief FileDownload allows to download binary files from the web into local files.
+ * Multiple downloads are queued and executed sequentially.
+ * An individual download is asynchrounous and chunked, i.e. the data is not completely read into memory but downloaded
+ * in chunks.
+ */
 class FileDownload : public QObject {
     Q_OBJECT
 
@@ -38,32 +44,78 @@ class FileDownload : public QObject {
 
     ~FileDownload();
 
+    /**
+     * @brief Check if there's enough free disk space and if the download location is writable.
+     * @param path Download location
+     * @param requiredMB Minimum of free disk space in megabyte
+     * @return true if download location is usable, false otherwise
+     */
     bool checkDiskSpace(const QDir &path, int requiredMB);
-    bool download(const QUrl &downloadUrl, const QDir &destinationDir, const QString &fileName);
 
-    bool                        hasError() const { return m_fileError != 0 || m_networkError != 0; }
-    QFileDevice::FileError      fileError() const { return m_fileError; }
-    QNetworkReply::NetworkError networkError() const { return m_networkError; }
-    QString                     errorString() const { return m_errorString; }
+    /**
+     * @brief Download the given file in downloadURL and store it in the given local file.
+     * The download is queued and each download is processed sequentially. Download feedback is given through the public
+     * signals.
+     * @param downloadUrl The download URL
+     * @param destinationDir The destination directory where to store the file
+     * @param fileName The filename to use for the download
+     * @param requiredFreeMB Check if there's enough free disk space in megabyte if value > 0
+     * @return Download identifier used for the signals
+     */
+    int download(const QUrl &downloadUrl, const QDir &destinationDir, const QString &fileName, int requiredFreeMB = 0);
 
  signals:
-    void downloadProgress(qint64 bytesReceived, qint64 bytesTotal);
-    void downloadSpeed(const QString &speed);
-    void downloadComplete();
-    void downloadFailed();
+    /**
+     * @brief This signal is emitted to indicate the progress of the current download.
+     * @param id The download identifier
+     * @param bytesReceived The number of bytes received
+     * @param bytesTotal Total number of bytes expected to be downloaded
+     * @param speed A human readable download speed label, e.g. "1.2 MB/s"
+     * @see https://doc.qt.io/qt-5/qnetworkreply.html#downloadProgress
+     */
+    void downloadProgress(int id, qint64 bytesReceived, qint64 bytesTotal, const QString &speed);
+
+    /**
+     * @brief This signal is emitted after a download has been successfully downloaded.
+     * @param id The download identifier
+     */
+    void downloadComplete(int id);
+
+    /**
+     * @brief This signal is emitted if a download failed.
+     * @param id The download identifier
+     * @param errorMsg A human-readable description of the error
+     */
+    void downloadFailed(int id, QString errorMsg);
+
+    /**
+     * @brief Signals that the last download has been finished and that there are no more downloads in the queue.
+     */
+    void downloadQueueEmpty();
 
  private slots:  // NOLINT open issue: https://github.com/cpplint/cpplint/pull/99
+    void startNextDownload();
     void onDownloadProgress(qint64 bytesReceived, qint64 bytesTotal);
     void onDownloadError(QNetworkReply::NetworkError error);
     void onDownloadFinished();
 
  private:
-    QNetworkAccessManager m_manager;
-    QFile                 m_file;
-    QNetworkReply *       m_download = nullptr;
-    QElapsedTimer         m_downloadTimer;
+    struct Download {
+     public:
+        int     id;
+        QUrl    url;
+        QDir    destinationDir;
+        QString fileName;
+        int     requiredMb;
+    };
 
-    QFileDevice::FileError      m_fileError;
-    QNetworkReply::NetworkError m_networkError;
-    QString                     m_errorString;
+    bool prepareFileDownload(const Download &download);
+
+    int                   m_downloadId = 0;
+    int                   m_currentDownloadId = 0;
+    QNetworkAccessManager m_manager;
+    QQueue<Download>      m_downloadQueue;
+    QFile                 m_outputFile;
+    QNetworkReply *       m_currentReply = nullptr;
+    QElapsedTimer         m_downloadTimer;
 };
