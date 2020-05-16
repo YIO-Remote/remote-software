03e7bf3d37105ab3e7d12d5227f5b8ae4736e4c8
Marton <hello@martonborzak.com>
2020-05-16 18:36:41 +0200

    fix: added `DOCK` back to supported integrations

--- sources/yioapi.h
+++ sources/yioapi.h
@@ -55,9 +55,9 @@ class YioAPI : public YioAPIInterface {
     QVariantMap getConfig() override;
     bool        setConfig(QVariantMap config);
 
-    bool addEntity(QVariantMap entity);
-    bool updatEntity(QVariantMap entity);
-    bool removeEntity(QString entityId);
+    bool addEntity(QVariantMap entity) override;
+    bool updatEntity(QVariantMap entity) override;
+    bool removeEntity(QString entityId) override;
 
     bool addIntegration(QVariantMap integration);
     bool updateIntegration(QVariantMap integration);
